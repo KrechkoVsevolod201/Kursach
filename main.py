82a1c4229ea60ae1712d934f8d796436861c2af2
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2022-11-09 00:27:48 +0400

    Теперь всё экспортируется в ексель файлик

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import xlsxwriter
+# Если автоимпорт не работает, то надо прописать в терминале "pip install XlsxWriter"
 
 
 def half_method():
@@ -9,7 +11,12 @@ def half_method():
     eps = 0.00001
     root = []
     fz = lambda z: (np.tan(z) - 1/z)
-
+    # открываем новый файл на запись
+    workbook = xlsxwriter.Workbook('z.xlsx')
+    # создаем там "лист"
+    worksheet = workbook.add_worksheet()
+    # в ячейку A1 пишем текст
+    worksheet.write('A1', 'Корни z')
     while n > 0:
         a = a1
         b = b1
@@ -20,15 +27,26 @@ def half_method():
             else:
                 a = c
         root.insert(100 - n, c)
+        # в ячейку A пишем текст
+        worksheet.write('A' + str(100-n+2), c)
         a1 = a1 + np.pi
         b1 = b1 + np.pi
         n = n - 1
-        #print(c)
-    print(root)  # Press Ctrl+F8 to toggle the breakpoint.
+    print(root)
+
+    # сохраняем и закрываем
+    workbook.close()
 
 
-# Press the green button in the gutter to run the script.
+def plotter():
+    fz1 = lambda z: (np.tan(z))
+    fz2 = lambda z: (-1/z)
+    z = np.linspace(-10, 10, 100000)
+    plt.plot(z, fz1(z))
+    plt.plot(z, fz2(z))
+    plt.grid(True)
+    plt.show()
+
+
 if __name__ == '__main__':
     half_method()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
