f8b8bfc96f084b5d9833859a98c22e85860764a5
Lilpank <70665037+Lilpank@users.noreply.github.com>
2022-11-15 00:56:08 +0300

    add P_n

--- main.py
+++ main.py
@@ -1,42 +1,62 @@
-import csv
-import pandas as pd
 import numpy as np
+import pandas as pd
+import sympy
+from sympy import symbols, integrate
 
 eps = 0.00001
 
+α = 0.002  # Вт/(см^2*град)
+c = 1.65  # Дж/(cм^3*град)
+l = 12  # см
+T = 250  # с
+k = 0.59  # Вт/(см*град)
+R = 1  # ? Узанть коэф-т
 
-def φ_n(μ: list) -> np.ndarray:
-    return 4 * 16 * np.sin(2 * np.array(μ)) / (12 * np.array(μ) + 2 * np.sin(np.array(μ) * 12))
+
+def φ_n(z: list) -> np.array:
+    return np.array(16 * np.sin(4 * np.array(z) / l) / (np.array(z) + np.sin(2 * np.array(z))))
 
 
-def half_method(n: int, a1: float, b1: float) -> list:
-    μ = list()
+def P_n(z, φ, t1=0, t_=1):
+    t2 = T / 10
+    t = symbols('t')
+    return φ * sympy.exp(4 * (z ** 2) / c / (l ** 2) * (k + 2 * α / (c * R))) * integrate(sympy.exp(t), (t, t1, t2)) \
+           - φ * integrate(t, (t, t1, t2)) * sympy.exp(-4 * (z ** 2) / c / (l ** 2) * (k + 2 * α / (c * R)) * t_)
+
+
+def half_method(n: int, a1: float, b1: float) -> np.ndarray:
+    z = list()
 
     def find_c(a: float, b: float) -> float:
-        G = 1
+
+        G = α / (c * l)
         fz = lambda z: (np.tan(z) - G / z)
-        c = (a + b) / 2
+        root = (a + b) / 2
         while np.abs(a - b) > eps:
-            if (fz(c) * fz(a)) < 0:
-                b = c
+            if (fz(root) * fz(a)) < 0:
+                b = root
             else:
-                a = c
-            c = (a + b) / 2
-        return c
+                a = root
+            root = (a + b) / 2
+        return root
 
     while n > 0:
-        c = find_c(a1, b1)
-        μ.append(c)
+        root = find_c(a1, b1)
+        z.append(root)
         a1 += np.pi
         b1 += np.pi
         n -= 1
-    return μ
+    return np.array(z)
 
 
 if __name__ == '__main__':
-    μ = half_method(100, 0.000001, np.pi / 2)
-    φ = φ_n(μ)
+    n = 10
+    z = half_method(n, 0.000001, np.pi / 2)
+    φ = φ_n(z)
 
-    df = pd.DataFrame({'μ': μ, 'φ': φ})
+    df = pd.DataFrame({'z': z, 'φ': φ})
+    df.index = df.index + 1
     print(df.to_string())
     df.to_csv('values.csv')
+    print()
+    print(P_n(z, φ))
