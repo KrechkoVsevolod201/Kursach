85368675812891b9904cd271f02cfab0d0b5362d
Lilpank <70665037+Lilpank@users.noreply.github.com>
2022-12-02 10:46:13 +0300

    графики

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sympy
-from sympy import N
-from multiprocessing.pool import ThreadPool
+from numba import njit, prange
 
 eps = 0.00001
 
@@ -15,14 +14,13 @@ k = 0.59  # Вт/(см*град)
 R = 0.1
 
 
-def φ_n(z: list) -> np.array:
-    z = np.array(z)
-
+@njit(cache=True, nogil=True)
+def φ_n(z) -> np.array:
     x1, x2 = 6, 8
-    return np.array(
-        4 * 16 * (np.sin((x2 - l / 2) * 2 * z / l) - np.sin((x1 - l / 2) * 2 * z / l)) / (2 * z + np.sin(2 * z)))
+    return 4 * 16 * (np.sin((x2 - l / 2) * 2 * z / l) - np.sin((x1 - l / 2) * 2 * z / l)) / (2 * z + np.sin(2 * z))
 
 
+@njit(cache=True, nogil=True)
 def half_method(n: int, a1: float, b1: float) -> np.ndarray:
     z = list()
 
@@ -47,78 +45,95 @@ def half_method(n: int, a1: float, b1: float) -> np.ndarray:
     return np.array(z)
 
 
-a2 = (k / c) ** 2
-aRc = (2 * α / R) / (c ** 2)
-Rn = lambda n: 128 / ((c * l ** 2) * ((n + 1 / 2) ** 2) * sympy.pi ** 3) / α ** 2
-q = lambda zi: a2 * 4 * (zi ** 2) / (l ** 2)
-
 hx = 1 / 10
 x_list = np.arange(0.0, l + hx, hx)
+ht = 1 / 10
+time_list = np.arange(0.0, T + ht, ht)
 
 
-def w_n(z, φ, time=1):
+@njit(nogil=True, cache=True)
+def w_n(z, φ, time=T, flag='x', x=l / 2):
     def P_n(zi, φi, t):
         a2 = (k / c) ** 2
         aRc = (2 * α / R) / (c ** 2)
-        q = lambda zi: a2 * 4 * (zi ** 2) / (l ** 2)
 
-        return (φi * 4 * (1 - sympy.exp(-1 * ((q(zi) + aRc) * t))) / (q(zi) + aRc)) / (c * l)
+        return (φi * 4 * (1 - np.exp(-1 * ((a2 * 4 * (zi ** 2) / (l ** 2) + aRc) * t))) / (
+                a2 * 4 * (zi ** 2) / (l ** 2) + aRc)) / (c * l)
 
     def w(z, φ, ti, x):
         s = list()
+        for i in prange(len(z)):
+            s.append(P_n(z[i], φ[i], ti) * np.cos(((2 * z[i] / l) * (x - l / 2))))
 
-        for zi, φi in zip(z, φ):
-            s.append(N(P_n(zi, φi, ti) * sympy.cos(((2 * zi / l) * (x - l / 2)))))
-        return sum(s)
+        return np.sum(np.array(s))
 
-    s = list(map(lambda xi: w(z, φ, time, xi), x_list))
+    sol = []
+    if flag == 'x':
+        for i in x_list:
+            sol.append(w(z, φ, time, i))
+    elif flag == 't':
+        for i in time_list:
+            sol.append(w(z, φ, i, x))
 
-    return s
-
-
-def plotter(results):
-    fig, ax = plt.subplots()
-    for i in results:
-        ax.plot(x_list, i)
-    ax.grid()
-
-    ax.set_xlabel('Координата Х, См')
-    ax.set_ylabel('Температура w, Кельвин')
-    plt.show()
+    return sol
 
 
-def solutions(n, t):
-    Rn = lambda n: 128 / ((c * l ** 2) * ((n + 1 / 2) ** 2) * sympy.pi ** 3) / α ** 2
+@njit(cache=True, nogil=True)
+def solutions(n, t=T, flag='x', x=l / 2):
+    def Rn(n):
+        return 128 / ((c * l ** 2) * ((n + 1 / 2) ** 2) * np.pi ** 3) / α ** 2
+
     # while True:
     z = half_method(n, 0.000001, np.pi / 2)
     φ = φ_n(z)
-    df = pd.DataFrame({'z': z, 'φ': φ})
-    df.index = df.index + 1
+    # df = pd.DataFrame({'z': z, 'φ': φ})
+    # df.index = df.index + 1
     # print(df.to_string())
-    df.to_csv('values.csv')
-    solution = w_n(z, φ, t)
-    # if N(Rn(n + 1)) / sum(solution) < eps:
+    # df.to_csv('values.csv')
+    solution = w_n(z, φ, t, flag=flag, x=x)
+    # if Rn(n + 1) / sum(solution) < eps:
     #     break
     # n += 1
+    # print(t, n)
     return solution
+
+def plotter(results_x, results_t):
+    plt.figure(1)
+    line = list()
+    for key, value in results_x.items():
+        line += plt.plot(x_list, value, label="t=" + str(key) + " с")
+    plt.xlabel('Координата Х, См')
+    plt.ylabel('Температура w, Кельвин')
+    plt.legend(line, [l.get_label() for l in line], loc=0)
+    plt.grid()
+
+    plt.figure(2)
+    line = list()
+    for key, value in results_t.items():
+        line += plt.plot(time_list, value, label="x=" + str(key) + " см")
+    plt.xlabel('Время T, секунда')
+    plt.ylabel('Температура w, Кельвин')
+    plt.legend(line, [l.get_label() for l in line], loc=0)
+    plt.grid()
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    numOfThreads = 7
-    results = []
-    pool = ThreadPool(numOfThreads)
-    results.append(pool.apply_async(solutions, (350, 500)))
-    results.append(pool.apply_async(solutions, (300, 400)))
-    results.append(pool.apply_async(solutions, (300, 350)))
-    results.append(pool.apply_async(solutions, (238, 250)))
-    results.append(pool.apply_async(solutions, (120, 200)))
-    results.append(pool.apply_async(solutions, (50, 150)))
-    results.append(pool.apply_async(solutions, (30, 100)))
+    t0 = time.time()
+    n = 50
+    t = 5
+    numOfThreads = 8
+    results_x = {}
+    results_t = {}
 
-    results = [r.get() for r in results]
+    for i in prange(numOfThreads):
+        results_x[(t + 35 * i)] = solutions(n + 35 * i, t + 35 * i)
+    for i in prange(numOfThreads - 1):
+        results_t[i] = solutions(n, flag='t', x=i)
 
-    pool.close()
-    pool.join()
+    t1 = time.time()
+    total = t1 - t0
+    print(total)
 
-    print(results)
-    plotter(results)
+    plotter(results_x, results_t)
